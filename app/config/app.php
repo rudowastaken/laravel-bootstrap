b610c20dd010d95ca5b2cc1ce29c079e6795dde6
HR <mail@hrcc.sk>
2014-02-11 00:00:21 +0100

    Added support for clockwork

--- app/config/app.php
+++ app/config/app.php
@@ -108,6 +108,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
 		'JasonNZ\LaravelGrunt\LaravelGruntServiceProvider',
+		'Clockwork\Support\Laravel\ClockworkServiceProvider',
 
 	),
 
@@ -174,6 +175,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Clockwork' 	  => 'Clockwork\Support\Laravel\Facade',
 
 	),
 
